293df20cd110e411d04fd935a31c11c8a3972e78
legluondunet <legluondunet@free.fr>
2019-09-29 14:25:38 +0200

    modifié :         scripts/changecase.sh

--- scripts/changecase.sh
+++ scripts/changecase.sh
@@ -2,15 +2,31 @@
 
 folder="$1"
 case="$2"
-cd "$folder"
 
-if $case!=[0,1]
-echo "la variable case n'est égale ni à 0 ni à 1"
-exit
-elif $case=0
-#find .  -depth -type d,f -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
-echo "la variable case est égale à " $case ", tout convertir en minuscule"
-elif $case=1
-#find .  -depth -type d -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[A-Z]" "[a-z]"); mv -f "$s" "$d"';
-echo "la variable case est égale à " $case ", tout convertir en majuscule"
+
+if cd "$folder"
+then
+    	echo "le répertoire $folder existe"
+	chmod -R +w "$folder"
+	ls -l "$folder"
+else
+	echo "répertoire non trouvé, arrêt du script"
+	ls -l
+	exit
 fi
+
+if [ $case != 0 ] && [ $case != 1 ]
+then
+	echo "la variable case n'est égale ni à 0 ni à 1, arret du script"
+	exit
+elif [ $case = 0 ]
+then
+find .  -depth -type d,f -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[A-Z]" "[a-z]"); mv -f "$s" "$d"';
+	echo "la variable case est égale à " $case ", convertir le répertoire " $folder " en minuscule"
+elif [ $case = 1 ]
+then
+find .  -depth -type d,f -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
+	echo "la variable case est égale à " $case ", convertir le répertoire " $folder "  en majuscule"
+fi
+
+
