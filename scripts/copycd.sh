c256f61395393222fa2a867007c2e48d7d9ad6a1
legluondunet <legluondunet@free.fr>
2019-11-13 21:15:16 +0100

    modifié :         scripts/copycd.sh

--- scripts/copycd.sh
+++ scripts/copycd.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cdname=$1
-
-drive=$(mount |grep /$cdname |cut -d " " -f 1)
-
-echo $drive $cdname
-
 if ! [ -x "$(command -v cdrdao)" ]; then
   echo 'Error: cdrdao is not installed.'
 exit
@@ -15,7 +9,22 @@ elif
 exit
 fi
 
-rm -f -r $cdname*
+cdname=$1
 
-cdrdao read-cd --datafile $cdname.bin --driver generic-mmc:0x00020000 --device $drive $cdname.toc
+cddev1=$(mount |grep /$cdname |cut -d " " -f 1)
+
+if [[ $cddev1 == *"/dev/sr"* ]]; then
+cddev=$(./lsscsi -g |grep $cddev1 | tail -c -10)
+else
+cddev=$cddev1
+fi
+
+echo "les variables --> cdname:$cdname cddev1:$cddev1 cddev:$cddev"
+
+rm -f -r $cdname.*
+
+echo "cdrdao read-cd --datafile $cdname.bin --driver generic-mmc:0x00020000 --device $cddev $cdname.toc"
+cdrdao read-cd --datafile $cdname.bin --driver generic-mmc:0x00020000 --device $cddev $cdname.toc
+
 toc2cue $cdname.toc $cdname.cue
+rm -f -r $cdname.toc lsscsi
