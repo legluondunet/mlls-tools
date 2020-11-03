4cdda58757876ce35f807e3f9e217ee5d5f9b463
legluondunet <legluondunet@free.fr>
2020-11-03 23:48:54 +0100

    modifié :         scripts/audiotools.sh

--- scripts/audiotools.sh
+++ scripts/audiotools.sh
@@ -3,21 +3,19 @@
 cdname=$1
 chemin=$2
 
-cddev=$(mount |grep -i $cdname |cut -d " " -f 1)
-
-echo "les variables --> cdname:$cdname cddev:$cddev"
-
-wget https://github.com/legluondunet/mlls-tools/raw/master/audiotools/audiotools.tar.xz
-
-tar xfv audiotools.tar.xz
-
-LD_LIBRARY_PATH=libs ./cdparanoia -BE -d $cddev
-
 if [ ! -d "$chemin" ] 
 then
 echo création du chemin $chemin
 mkdir -p "$chemin"
 fi
+
+cd "$chemin"
+wget https://github.com/legluondunet/mlls-tools/raw/master/audiotools/audiotools.tar.xz
+tar xfv audiotools.tar.xz
+
+cddev=$(mount |grep -i $cdname |cut -d " " -f 1)
+echo "les variables --> cdname:$cdname cddev:$cddev"
+LD_LIBRARY_PATH=libs ./cdparanoia -BE -d $cddev
 
 n=1
 for f in *.wav 
@@ -31,5 +29,5 @@ echo convert $final to flac
 done
 mv *.flac "$chemin"
 
-#rm -f -r cdparanoia flac metaflac libs audiotools.sh about_audiotools.txt audiotools.tar.xz
+rm -f -r cdparanoia flac metaflac libs audiotools.sh about_audiotools.txt audiotools.tar.xz
 
